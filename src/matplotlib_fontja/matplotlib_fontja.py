c9494e3073ca6997d3e3615039b3fd9ecf745a0c
Ciffelia <mc.prince.0203@gmail.com>
2024-01-03 17:28:22 +0900

    Python 3.12で削除されたdistutilsを使わないよう修正

--- src/matplotlib_fontja/matplotlib_fontja.py
+++ src/matplotlib_fontja/matplotlib_fontja.py
@@ -2,7 +2,6 @@ import os
 
 import matplotlib
 from matplotlib import font_manager
-from distutils.version import LooseVersion
 
 FONTS_DIR = 'fonts'
 FONT_NAME = "IPAexGothic"
@@ -13,13 +12,12 @@ def japanize():
     font_dir_path = get_font_path()
     font_dirs = [font_dir_path]
     font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
-    is_support_createFontList = LooseVersion(matplotlib.__version__) < '3.2'
-    if is_support_createFontList:
-        font_list = font_manager.createFontList(font_files)
-        font_manager.fontManager.ttflist.extend(font_list)
-    else:
+    if hasattr(font_manager.fontManager, 'addfont'):
         for fpath in font_files:
             font_manager.fontManager.addfont(fpath)
+    else:
+        font_list = font_manager.createFontList(font_files)
+        font_manager.fontManager.ttflist.extend(font_list)
     matplotlib.rc('font', family=FONT_NAME)
 
 
