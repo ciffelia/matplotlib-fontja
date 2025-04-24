177fba94eb5c7f7a944440797376ff76680fa6b1
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-04-24 21:36:28 +0900

    add type hints

--- src/matplotlib_fontja/matplotlib_fontja.py
+++ src/matplotlib_fontja/matplotlib_fontja.py
@@ -8,7 +8,7 @@ FONT_NAME = "IPAexGothic"
 FONT_TTF = 'ipaexg.ttf'
 
 
-def japanize():
+def japanize() -> None:
     font_dir_path = get_font_path()
     font_dirs = [font_dir_path]
     font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
@@ -21,11 +21,11 @@ def japanize():
     matplotlib.rc('font', family=FONT_NAME)
 
 
-def get_font_ttf_path():
+def get_font_ttf_path() -> str:
     return os.path.join(get_font_path(), FONT_TTF)
 
 
-def get_font_path():
+def get_font_path() -> str:
     return os.path.abspath(os.path.join(os.path.dirname(__file__), FONTS_DIR))
 
 
